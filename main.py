7a7d2f59d5fdf90f3cc56c110adb32a18ba96980
chaoyihu <chaoyihu@outlook.com>
2019-02-03 15:19:17 +0800

    添加了请求错误的判断

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import time
 from gradesjudge import gradesjudge
 from emailsend import emailsend, buildtext
+from pytesseract import TesseractError
+from requests import ConnectionError
+from smtplib import SMTPConnectError
 
 if __name__ == '__main__':
     print('--------------------南昌大学分数更新提示系统--------------------')
@@ -9,11 +12,24 @@ if __name__ == '__main__':
     password = input('请输入密码:')
     email = input('请输入获取提示邮件的邮箱(推荐使用QQ邮箱,example@qq.com):')
     while True:
-        waitsend = gradesjudge(username, password)
-        if waitsend is None:
-            print('等待3分钟后重新获取新数据...')
-            time.sleep(180)
-        else:
-            emailsend(buildtext(username, waitsend), email)
-            print('等待3分钟后重新获取新数据...')
-            time.sleep(180)
+        try:
+            waitsend = gradesjudge(username, password)
+            if waitsend is None:
+                print('等待3分钟后重新获取新数据...')
+                time.sleep(180)
+            else:
+                emailsend(buildtext(username, waitsend), email)
+                print('等待3分钟后重新获取新数据...')
+                time.sleep(180)
+        except TesseractError:
+            print('系统出错!请检查是否安装Tesseract-OCR并将其置于环境变量PATH!')
+            print('等待1分钟后重新获取新数据...')
+            time.sleep(60)
+        except ConnectionError:
+            print('系统出错!请检查网络是否通畅!')
+            print('等待1分钟后重新获取新数据...')
+            time.sleep(60)
+        except SMTPConnectError:
+            print('系统出错!请检查邮箱地址和授权码是否填写正确!')
+            print('等待1分钟后重新获取新数据...')
+            time.sleep(60)
